75b69e8ddbf649a6954ae60487a9e73e007b0b08
kmasanori <kmasanori0221@gmail.com>
2022-08-16 16:01:26 +0900

    feat: add product table and entity.

---
+++ src/Model/Table/ProductsTable.php
@@ -0,0 +1,61 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Model\Table;
+
+use Cake\ORM\Query;
+use Cake\ORM\RulesChecker;
+use Cake\ORM\Table;
+use Cake\Validation\Validator;
+
+/**
+ * Products Model
+ *
+ * @method \App\Model\Entity\Product newEmptyEntity()
+ * @method \App\Model\Entity\Product newEntity(array $data, array $options = [])
+ * @method \App\Model\Entity\Product[] newEntities(array $data, array $options = [])
+ * @method \App\Model\Entity\Product get($primaryKey, $options = [])
+ * @method \App\Model\Entity\Product findOrCreate($search, ?callable $callback = null, $options = [])
+ * @method \App\Model\Entity\Product patchEntity(\Cake\Datasource\EntityInterface $entity, array $data, array $options = [])
+ * @method \App\Model\Entity\Product[] patchEntities(iterable $entities, array $data, array $options = [])
+ * @method \App\Model\Entity\Product|false save(\Cake\Datasource\EntityInterface $entity, $options = [])
+ * @method \App\Model\Entity\Product saveOrFail(\Cake\Datasource\EntityInterface $entity, $options = [])
+ * @method \App\Model\Entity\Product[]|\Cake\Datasource\ResultSetInterface|false saveMany(iterable $entities, $options = [])
+ * @method \App\Model\Entity\Product[]|\Cake\Datasource\ResultSetInterface saveManyOrFail(iterable $entities, $options = [])
+ * @method \App\Model\Entity\Product[]|\Cake\Datasource\ResultSetInterface|false deleteMany(iterable $entities, $options = [])
+ * @method \App\Model\Entity\Product[]|\Cake\Datasource\ResultSetInterface deleteManyOrFail(iterable $entities, $options = [])
+ *
+ * @mixin \Cake\ORM\Behavior\TimestampBehavior
+ *
+ * NOTE: Table オブジェクトを ProductsTable と名付けることで、CakePHP は、命名規則により products テーブルを使用するモデルであると解釈します。
+ *       もし一致するファイルが src/Model/Table に見つけられなければ、CakePHP は動的に モデルオブジェクトを生成します。これはまた、不意に間違ったファイル名 (例えば、 articlestable.php や ArticleTable.php) をつけると、CakePHP はどの設定も認識できず、 代わりに生成されたモデルを使うことになるということも意味します。
+ */
+class ProductsTable extends Table
+{
+    public function initialize(array $config): void
+    {
+        // parent::initialize($config);
+
+        // $this->setTable('products');
+        // $this->setDisplayField('name');
+        // $this->setPrimaryKey('id');
+
+        $this->addBehavior('Timestamp');
+    }
+
+    public function validationDefault(Validator $validator): Validator
+    {
+        $validator
+            ->scalar('name')
+            ->maxLength('name', 255)
+            ->requirePresence('name', 'create')
+            ->notEmptyString('name');
+
+        $validator
+            ->scalar('description')
+            ->requirePresence('description', 'create')
+            ->notEmptyString('description');
+
+        return $validator;
+    }
+}
